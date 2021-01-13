9fc994cf695b7be907e04fb02bf537ddead61825
Alwiaulian <alwi.nuryadin@gmail.com>
2021-01-13 20:05:58 +0700

    13_01_21

---
+++ Instruktur_Alwi/restapi/app/models/customer.model.js
@@ -0,0 +1,40 @@
+const sql = require("./db.js");
+
+// constructor
+const Customer = function(customer) {
+  this.email = customer.email;
+  this.name = customer.name;
+  this.active = customer.active;
+};
+
+Customer.create = (newCustomer, result) => {
+  sql.query("INSERT INTO customers SET ?", newCustomer, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created customer: ", { id: res.insertId, ...newCustomer });
+    result(null, { id: res.insertId, ...newCustomer });
+  });
+};
+
+Customer.findById = (customerId, result) => {
+  sql.query(`SELECT * FROM customers WHERE id = ${customerId}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found customer: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found Customer with the id
+    result({ kind: "not_found" }, null);
+  });
+};
