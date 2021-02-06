3490d05326ba8c34779a9afbc00e258b49b5153c
Encep Suryana <encep.suryanajr@gmail.com>
2021-02-06 09:53:16 +0700

    update

--- Instruktur/27_01_21/basic-create-table.js
+++ Instruktur/27_01_21/basic-create-table.js
@@ -15,10 +15,13 @@ koneksi.connect(function (err){
 
     console.log('Koneksi database berhasil!');
 
-    let query_sql = `CREATE TABLE IF NOT EXISTS customers(
+    let query_sql = `CREATE TABLE IF NOT EXISTS siswa(
                         id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                        nama_depan varchar(100) NOT NULL,
+                        nama_belakang varchar(100) NOT NULL,
+                        tgl_lahir DATE NOT NULL,
+                        alamat TEXT,
                         email varchar(255) NOT NULL,
-                        nama_lengkap varchar(255) NOT NULL,
                         aktif BOOLEAN DEFAULT false)`;
     koneksi.query(query_sql, function(err) {
        if (err) {
