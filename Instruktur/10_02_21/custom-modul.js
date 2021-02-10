b00519106d3bdb1e9cf44783dd06eb4f2206b796
Encep Suryana <encep.suryanajr@gmail.com>
2021-02-10 19:01:32 +0700

    10_02_21

---
+++ Instruktur/10_02_21/custom-modul.js
@@ -0,0 +1,3 @@
+var pesan1 = require('./modul-pesan');
+
+console.log(pesan1.pesan2() + ' ' + pesan1.pesan());
---
+++ Instruktur/10_02_21/modul-pesan.js
@@ -0,0 +1,7 @@
+exports.pesan = function() {
+    return 'Pesan dari modul-pesan.js';
+}
+
+exports.pesan2 = function() {
+    return 'Pesan2 dari modul-pesan.js';
+}
