ea8dcedf195f0164a5c0a16c1efc923af327c75e
Encep Suryana <encep.suryanajr@gmail.com>
2021-02-20 10:44:40 +0700

    20_02_21

---
+++ Instruktur/20_02_21/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app     = express();
+
+// app.get('/', function (req, res) {
+//     res.send('Salam dari Express Js!');
+// });
+
+// app.all('/all', function (req, res) {
+//     res.send('Salam dari ALL Express Js!');
+// });
+
+app.use(function (req, res, next){
+    console.log('Time : ', Date.now());
+    next();
+});
+
+
+app.use(function (req, res, next){
+    console.log('Request Type: ', req.method);
+    next();
+});
+
+app.get('/', function (req, res) {
+    console.log('Salam dari Express Js!');
+    res.send('Salam dari Express Js!');
+});
+
+app.get('/user/:id', function (req, res) {
+    console.log('Menampilkan User ID');
+    res.send('Menampilkan User ID');
+});
+
+
+const port = 8080;
+app.listen(port, function(){
+    console.log('Server Running di port: ' + port);
+});
