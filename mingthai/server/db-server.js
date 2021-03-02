284829b9e6a605f3cfab970063e44c048b701042
mingthai <mingthai@gmail.com>
2021-03-02 16:14:21 +0700

    02_03_21

---
+++ mingthai/server/db-server.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: ''
+});
+
+conn.connect(function(err) {
+   if(err){
+       console.error('Koneksi Error: '+err.stack);
+       return;
+   }
+   //Create Database
+   console.log('Terkoneksi dengan ID: '+conn.threadId);
+   
+   let query = `CREATE DATABASE nodejs`;
+
+   conn.query(query, function(err,result)){
+    if(err){
+        console.error('Koneksi Error: '+err.stack);
+        throw(err);
+    }   
+   
+    console.log('Database Node Js telah dibuat!');
+   conn.destroy();
+})
+
+});
