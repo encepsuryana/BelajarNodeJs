aa8aa655ecfb49f834e069a1baf67e52d8bf2fcd
mingthai <mingthai@gmail.com>
2021-03-13 11:56:21 +0700

    13_03_21

--- mingthai/server/db-server.js
+++ mingthai/server/db-server.js
@@ -34,25 +34,82 @@ conn.connect(function (err) {
     //              description VARCHAR(255)   
     //)`;
 
-//VAR INSERT TABLE
+    // //VAR INSERT TABLE
+    // //Variable Database
+    // let data_query = '1 Value';
+
+    // //Query Insert Value
+    // let query = `INSERT INTO module (title,description)
+    //              VALUES ('Node js', 'Dasar dasar Node Js'),
+    //                     ('Node js 1', 'Dasar dasar Node Js 1')`;
+
+    // //VAR ALTER TABLE ADD COLUMN
+    // //Variable Database
+    // let data_query = '1 Kolom ';
+
+    // //Query Insert Value
+    // let query = `ALTER TABLE module
+    //              ADD dummy varchar(11)`;
+
+
+
+    // //VAR ALTER TABLE MODIFY COLUMN FIRST
+    // //Variable Database
+    // let data_query = '1 Kolom ';
+
+    // //Query Insert Value
+    // let query = `ALTER TABLE module
+    //              MODIFY id int(11) 
+    //              FIRST `;
+   
+    // //VAR ALTER TABLE MODIFY COLUMN AFTER
+    // //Variable Database
+    // let data_query = '1 Kolom ';
+
+    // //Query Insert Value
+    // let query = `ALTER TABLE module
+    //              MODIFY COLUMN description varchar(255)
+    //              AFTER id`;
+            
+    // //VAR ALTER DROP COLUMN
+    // //Variable Database
+    // let data_query = '1 Kolom ';
+
+    // //Query Insert Value
+    // let query = `ALTER TABLE module
+    //              DROP COLUMN dummy`;
+
+    // //VAR UPDATE VALUE
+    // //Variable Database
+    // let data_query = '1 Value diubah';
+
+    // //Query Insert Value
+    // let query = `UPDATE module SET title = 'Node JS 0',Description = 'Dasar dasar Node Js 0'
+    //               WHERE id = 1`;
+
+
+    // //VAR DELETE VALUE
+    // //Variable Database
+    // let data_query = '1 Value dihapus';
+
+    // //Query Insert Value
+    // let query = `DELETE FROM module
+    //               WHERE id = 3`;
+
+    //VAR SELECT VALUE
     //Variable Database
-    let data_query = '1 Value';
+    let data_query = 'Menampilkan data';
 
-     //Query Insert Value
-    let query = `INSERT INTO module (title,description)
-                 VALUES ('Node js', 'Dasar dasar Node Js'),
-                        ('Node js 1', 'Dasar dasar Node Js 1')`;
+    //Query Insert Value
+    let query = `SELECT * FROM module`;
 
-    
-
-
-
-    conn.query(query, function (err, result) {
+    conn.query(query, function (err, result,fields) {
         if (err) {
             throw (err);
         }
 
         console.log(data_query + ' telah dibuat!');
+        console.log(result);
         conn.destroy();
     })
 });
